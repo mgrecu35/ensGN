23aec028d9f3d2a6c863bca0ad5cbef0f54f0c96
Mircea Grecu <mirgrecu@gmail.com>
2022-01-11 08:48:11 -0500

    neighbor init

--- simpleFModel_Ens.py
+++ simpleFModel_Ens.py
@@ -86,8 +86,36 @@ ind=range(zKa.shape[0])
 ind_train, ind_test, \
     y_train, y_test \
     = train_test_split(ind, pRate[:,0], \
-                       test_size=0.15, random_state=42)
+                       test_size=0.25, random_state=42)
 
+from sklearn.cluster import KMeans
+import matplotlib
+
+matplotlib.rcParams.update({'font.size': 12})
+
+kmeans = KMeans(n_clusters=9, random_state=0).fit(pRate[ind_train,:120])
+plt.figure(figsize=(9,9))
+pRateM=[]
+pRatem=pRate[ind_train,:120].mean(axis=0)
+for i in range(9):
+    a=np.nonzero(kmeans.labels_==i)
+    plt.subplot(3,3,i+1)
+    p1=pRate[ind_train,:120][a[0],:].mean(axis=0)
+    pRateM.append(p1*pRatem[0]/p1[0])
+    plt.plot(p1*pRatem[0]/p1[0],h[:120]/1000.,\
+             color='blue')
+    plt.plot(pRatem,h[:120]/1000.,'-.',color='blue')
+    if i%3==0:
+        plt.ylabel('Height(km)')
+    if i>=6:
+        plt.xlabel('Precip. rate(mm/h)')
+    plt.grid()
+    plt.title('Class %2i'%i)
+    plt.tight_layout()
+
+plt.savefig('avegProfiles_2.png')
+
+#stop
 covT=np.cov(zL[ind_train,:120].T,pRate[ind_train,:120].T)
 covYY_inv=np.linalg.pinv(covT[:120,:120]+np.eye(120)*9)
 kGain=covT[120:,:120]@covYY_inv
@@ -95,7 +123,8 @@ pRatem=pRate[ind_train,:120].mean(axis=0)
 zm=zL[ind_train,:120].mean(axis=0)
 x1,x2=[],[]
 from sklearn.neighbors import NearestNeighbors
-neigh = NearestNeighbors(n_neighbors=100)
+
+neigh = NearestNeighbors(n_neighbors=200)
 zthr=10
 zL0=zL.copy()
 zL0[zL0<zthr]=0
@@ -143,46 +172,59 @@ def g_fobj(x):
     return z_attd,z_att_f
 bnds=[(0, 100) for i in range(120)]
 
-for i in ind_test:
-    a0=np.nonzero(zL[i,:120]>10)
-    zobs=zL[i,:120]
-    
-    nEns=130
-    pRateEns = rng.multivariate_normal(pRatem, covT[120:,120:], (nEns))
-    #for it in range(2):
+from sklearn.neighbors import NearestNeighbors
+neigh = NearestNeighbors(n_neighbors=150)
+zthr=10
+nEns=6000
+xsc=np.diag([1.3 for i in range(120)])
+classes=[0,1,2,3,4,5,6,7,8]
+xc=[]
+for c in classes:
+    pRateEns = rng.multivariate_normal(pRateM[c], covT[120:,120:]@xsc, (nEns))
     pRateEns[pRateEns<0]=0
-    alpha=0.02
-    x=pRatem.copy()
-    fL=[f_obj(x)]
-    for it in range(300):
-        z_attd,z_att_f=g_fobj(x)
-        g_val=(z_attd[a0[0],:]).T@(z_att_f[a0]-zobs[a0])/4.0+\
-            10*(x-pRatem).T@B_inv
-        x-=alpha*g_val
-        x[x<0]=0
-        x[x>100]=100
-    fL.append(f_obj(x))
-    fL.append(f_obj(pRate[i,:120]))
-    print(fL)
-    
-    #stop
-    #z_att,z_attd = calg.computez_d(pRateEns[1,:],pRateD,fint[:120],\
-    #                               pZ_snow,pZ_rain,att_snow,att_rain,dr)
-    
-    #z_att_f = calg.computez(pRateEns[1,:],fint[:120],\
-    #                        pZ_snow,pZ_rain,att_snow,att_rain,dr)
+    zEns=computeZ(pRateEns[:,:120],fint[:120],pZ_snow,pZ_rain,\
+                  att_snow,att_rain)
+    zEns[zEns<zthr]=0
+    neigh.fit(zEns)
+    x1=[]
+    x2=[]
+    x3=[]
+    for i in ind_test:
+        a0=np.nonzero(zL[i,:120]>10)
+        zobs=zL[i,:120]
+        zobs0=zobs.copy()
+        zobs0[zobs0<zthr]=0
+        ind=neigh.kneighbors([zobs0])
+        ind=ind[-1][0]
+        x1.append(pRateEns[ind,0:120].mean(axis=0))
+        x2.append(pRate[i,0:120])
+        pRateEns_1=pRateEns[ind,:120].copy()
+        for it in range(2):
+            zEns_1=computeZ(pRateEns_1,fint[:120],pZ_snow,pZ_rain,\
+                        att_snow,att_rain)
+        zEns_1[zEns_1<zthr]=0
+        covT_ens=np.cov(zEns_1.T,pRateEns_1.T)
+        covYY_inv=np.linalg.pinv(covT_ens[:120,:120]+np.eye(120)*4*2)
+        kGainE=covT_ens[120:,:120]@covYY_inv
+        for ie in range(150):
+            pRateEns_1[ie,:]=pRateEns_1[ie,:]+\
+                kGainE[:,a0[0]]@(zL[i,a0[0]]-zEns_1[ie,:][a0])
+        pRateEns_1[pRateEns_1<0]=0
+        pRateEns1=pRateEns_1.mean(axis=0)
+        #pRateD=pRateEns1*0+1.0
+        #covXX=np.cov(pRateEns_1[:,:120].T)
+        #z_att_f, z_attd = calg.computez_d(pRateEns1,pRateD,fint[:120],\
+            #                        pZ_snow,pZ_rain,att_snow,att_rain,dr)
         
+        #A_1=np.linalg.pinv(z_attd@covXX@z_attd.T+np.eye(120)*4)
+        #kgain=covXX*z_attd.T@A_1
+        #pRateEns1+=kgain[:,a0[0]]@(zL[i,a0[0]]-z_att_f[a0[0]])
+        x3.append(pRateEns1[0:120])
+    xc.append([x1,x2,x3])
     #stop
-    #covT_ens=np.cov(zEns.T,pRateEns.T)
-    #covYY_inv=np.linalg.pinv(covT_ens[:120,:120]+np.eye(120)*4*2)
-    #kGainE=covT_ens[120:,:120]@covYY_inv
-    #print(pRateEns.mean(axis=0)[:3],pRateEns.std(axis=0)[:3])
-    #for ie in range(nEns):
-    #    pRateEns[ie,:]=pRateEns[ie,:]+\
-    #        0.5*kGainE[:,a0[0]]@(zL[i,a0[0]]-zEns[ie,a0[0]])
    
-
-    x1.append(x[0])
-    x2.append(pRate[i,0])
-   # x3.append(pRatem[0])
-stop
+xc=np.array(xc)
+for i in range(6):
+    print(np.corrcoef(xc[i,0,:,0],xc[i,1,:,0])[0,1],\
+          np.corrcoef(xc[i,1,:,0],xc[i,2,:,0])[0,1])
+    print(xc[i,:,:,0].mean(axis=-1))
